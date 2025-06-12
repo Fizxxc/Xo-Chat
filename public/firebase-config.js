1aa50aeeb32ca4cfddddce99a9f582b148745b39
Fizxxc <fizzx909@gmail.com>
2025-06-12 15:48:26 +0700

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,9 @@
-// firebase-config.js (gunakan <script type="module"> untuk menggunakannya)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-database.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-analytics.js";
-// Konfigurasi Firebase
- const firebaseConfig = {
+// public/firebase.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
+import { getDatabase, ref, push, onValue, onChildAdded, set } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
+
+const firebaseConfig = {
     apiKey: "AIzaSyBlNHkA1f-1GwBN0nBchMtIwEYUNLlq8FQ",
     authDomain: "e-commerce-a6fe2.firebaseapp.com",
     databaseURL: "https://e-commerce-a6fe2-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -14,10 +13,9 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.0/firebase
     appId: "1:169688929056:web:8d04f0b02c98fa77d1bd45",
     measurementId: "G-Q8FP7FQQHV"
   };
-// Inisialisasi Firebase
+
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = getDatabase(app);
-const analytics = getAnalytics(app);
-// Export supaya bisa diakses oleh modul lain
-export { app, auth, db };
+const auth = getAuth(app);
+
+export { db, auth, ref, push, onValue, onChildAdded, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, set };
